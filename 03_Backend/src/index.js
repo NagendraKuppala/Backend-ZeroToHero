8b674e0ac2da098eae1e3efa61b23fc25669cfae
NagendraKuppala <nagendrakumar.kuppala@gmail.com>
2023-12-14 15:46:15 -0800

    MongoDB Connection Setup

---
+++ 03_Backend/src/index.js
@@ -0,0 +1,36 @@
+//import mongoose from "mongoose";
+//import { DB_NAME } from "./constants";
+import dotenv from "dotenv"
+
+dotenv.config({
+    path: "./.env"
+})
+
+// Approach_2: Import DB Connectoin from 'db' for better readability 
+import connectDB from "./db/index.js";
+connectDB()
+
+// Approach_1: Entire DB Connection codebase in Index file.
+/*
+import express from "express";
+const app = express();
+// IIFE function '()()': Immidiately Invoke to connect DB
+;( async () => {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+        app.on("error", (error)=>{
+            console.error("MongoDB Connection Error: ", error)
+            throw error
+        })
+
+        app.listen(process.env.PORT, ()=>{
+            console.log(`Server is running on port ${process.env.PORT}`);
+        })
+
+    } catch(error){
+        console.error(`MongoDB Connection Error: ${error}`)
+        throw error
+    }
+})()
+*/
